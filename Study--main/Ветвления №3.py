92e6dd3ffd92149da270f802d553e897be9cf8ce
Aleksey8141 <alekseyoleksa@gmail.com>
2022-07-12 22:01:42 +0300

    Good

--- Study--main/Ветвления №3.py
+++ Study--main/Ветвления №3.py
@@ -11,8 +11,18 @@ x1 и x2
 x1 + ix2 и x1 + ix2
 '''
 
-a =
-b =
-c =
+from math import sqrt, fabs
+
+a = 2
+b = 4
+c = 10
 
 D = b**2 - 4*a*c
+if  D >= 0:
+    x1 = (-b + sqrt(D))/ (2 * a)
+    x2 = (-b - sqrt(D))/ (2 * a),
+    print(round(x1, 2), round(x2, 2))
+else:
+    x1 = -b/2*a
+    x2 = sqrt(fabs(D)) / (2 * a)
+    print(complex(x1, x2), complex(x1, -x2))
