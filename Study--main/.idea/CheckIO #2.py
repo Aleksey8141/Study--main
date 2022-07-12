0be86ca411c74e94bcae62410a194d221a76cf24
Aleksey8141 <alekseyoleksa@gmail.com>
2022-07-12 21:12:56 +0300

    Good

--- Study--main/.idea/CheckIO #2.py
+++ Study--main/.idea/CheckIO #2.py
@@ -4,12 +4,11 @@
 Строка состоит только из английских символов и пробелов.
 В начале и в конце строки пробелов нет.
 '''
-
 def first_word(text):
-    text = 'Hello world'
-    text = text.split(' ')[0]
-    #del text[1::]
-    return text
+    text = 'greeting from CheckiO Planet'
+    text = text.split(' ')
+    # del text[1::] you can take first element instead deleting other
+    return text[0]
 first_word('')
 print(first_word(''))
 
@@ -17,3 +16,16 @@ assert first_word('Hello world') == 'Hello'
 assert first_word('a word') == 'a'
 assert first_word('greeting from CheckiO Planet') == 'greeting'
 assert first_word('hi') == 'hi'
+
+#def first_word(text):
+#    text = 'Hello world'
+#    text = text.split(' ')[0]
+#    #del text[1::]
+#    return text
+#first_word('')
+#print(first_word(''))
+
+#assert first_word('Hello world') == 'Hello'
+#assert first_word('a word') == 'a'
+#assert first_word('greeting from CheckiO Planet') == 'greeting'
+#assert first_word('hi') == 'hi'
---
+++ Study--main/CheckIO.py
@@ -0,0 +1,29 @@
+'''
+Напишите функцию, которая будет получать 2 числа и возвращать результат произведения этих чисел.
+
+--------
+assert mult_two(3, 2) == 6
+assert mult_two(0, 1) == 0
+'''
+
+def mult_two(a, b):
+    mult = a*b
+    return mult
+mult_two(3, 2)
+print(mult_two(3, 2))
+    #print('Mult_two1:', '=', a*b)
+assert mult_two(3, 2) == 6
+assert mult_two(0, 1) == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
